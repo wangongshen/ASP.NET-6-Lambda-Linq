152dc37b6d8e62624709c7cc0146de50c708ef69
971901557@qq.com <971901557@qq.com>
2019-09-13 16:30:25 +0800

    泛型委托

--- 委托/Program.cs
+++ 委托/Program.cs
@@ -15,7 +15,10 @@ namespace 委托
         //声明无参委托
         public delegate void MyDelegate();
         //声明有参委托
-        //public delegate void MyDelegate(int num);
+        public delegate void MyDelegate01(int Num);
+        public delegate void MyDelegate02(string str);
+        public delegate void MyDelegate03<T>(T del);
+
 
         static void Main(string[] args)
         {
@@ -27,6 +30,16 @@ namespace 委托
             Console.WriteLine("654321");
             Console.WriteLine("------------将委托用参数方式传递---------------");
             DelegateCC(md);
+            Console.WriteLine("------------Action委托（非泛型）-----------------------------");
+            Action action = show;
+            action();
+            Console.WriteLine("------------Action委托（泛型）--有参数无返回值---------------------------");
+            Action<string> action1 = show; //不用声明直接能用
+            Action<int> action2 = show;
+            action1("string");
+            action2(123); 
+            Console.WriteLine("------------Func委托（泛型）----------------------------");
+
 
         }
 
@@ -36,6 +49,19 @@ namespace 委托
             Console.WriteLine("执行方法");
         }
 
+        public static void show(string str)
+        {
+            Console.WriteLine("执行方法:"+str);
+        }
+
+        public static void show(int num)
+        {
+            Console.WriteLine("执行方法:"+num);
+        }
+        public static void show(bool bl)
+        {
+            Console.WriteLine("执行方法:" + bl);
+        }
         public static void DelegateCC(MyDelegate delagate)
         {
             Console.WriteLine("1111111111");
